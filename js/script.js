808f4f5e992adff8ffbc6e7ae08509c709d7778f
Jean Lunde <jean.lunde@gmail.com>
2019-12-11 10:42:43 -0600

    Added chart type images and splitting code.

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@ $(document).ready(function () {
         $('img', this).toggle();
     });
 });
+
+console.clear();
+
+// Splitting js begins here.
+Splitting({
+  target: '.tiler',
+  by: 'cells',
+  rows: 3,
+  columns: 3,
+  image: true
+}); //Image('.tiler-overlay', { rows: 3, cols: 3 });
