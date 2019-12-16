c5a66592d0f67e53407770dac7f26e8e34560ed7
Jean Lunde <yn5283ne@e2381-as1665816.century.edu>
2019-12-16 16:35:12 -0600

    Started adding script for fades on the png images in the chartDelucluttering section.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
 $(document).ready(function () {
   $("#charactersAll").fadeIn(5000);
-  $("#charactersSelective").delay(2000).fadeIn(6000);
+  $("#charactersSelective").delay(0).fadeIn(6000);
   $("#beforeAfterCharts").click(function () {
     $('img', this).fadeToggle();
   });
+  $("#original").fadeOut(2000);
+  $("#step1").fadeIn(2000);
+  $("#step1").fadeOut(2000);
+  $("#step2").fadeIn(2000);
+  $("#step2").fadeOut(2000);
+  $("#step3").fadeIn(2000);
+  $("#step3").fadeOut(2000);
+  $("#step4").fadeIn(2000);
+  $("#step4").fadeOut(2000);
+  $("#step5").fadeIn(2000);
+  $("#step5").fadeOut(2000);
+  $("#step6").fadeIn(2000);
+  $("#step6").fadeOut(2000);
+  $("#finished").fadeIn(2000);
 });
+
+
+
+
+
 
 console.clear();
 
