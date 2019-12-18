bdaf8cb8a983bd12c78efd298893e797234ca0d9
Jean Lunde <yn5283ne@e2381-as1665816.century.edu>
2019-12-18 12:59:11 -0600

    Added button event interaction to chartDecluttering section and deleted an unneeded png image

--- js/script.js
+++ js/script.js
@@ -1,27 +1,55 @@
 $(document).ready(function () {
-  $("#charactersAll").fadeIn(5000);
-  $("#charactersSelective").delay(0).fadeIn(6000);
+  $("#charactersAll").fadeIn(4000);
+  $("#charactersSelective").delay(0).fadeIn(5000);
   $("#beforeAfterCharts").click(function () {
     $('img', this).fadeToggle();
   });
-  $("#original").fadeOut(2000);
-  $("#step1").fadeIn(2000);
-  $("#step1").fadeOut(2000);
-  $("#step2").fadeIn(2000);
-  $("#step2").fadeOut(2000);
-  $("#step3").fadeIn(2000);
-  $("#step3").fadeOut(2000);
-  $("#step4").fadeIn(2000);
-  $("#step4").fadeOut(2000);
-  $("#step5").fadeIn(2000);
-  $("#step5").fadeOut(2000);
-  $("#step6").fadeIn(2000);
-  $("#step6").fadeOut(2000);
-  $("#finished").fadeIn(2000);
+
+  $(".button").addClass(".buttonA").click(function () {
+    $("#original").fadeOut(1000, function () {
+      $("#step1").fadeIn(1000);
+    })
+  });
+
+  $(".buttonA").addClass(".buttonB").click(function () {        
+    $("#step1").fadeOut(1000, function () {
+      $("#step2").fadeIn(1000);
+    })
+  });
+
+  $(".buttonB").addClass(".buttonC").click(function () {
+    $("#step2").fadeOut(1000, function () {
+      $("#step3").fadeIn(1000);
+    })
+  });
+
+  $(".buttonC").addClass(".buttonD").click(function () {
+    $("#step3").fadeOut(1000, function () {
+      $("#step4").fadeIn(1000);
+    })
+  });
+
+  $(".buttonD").addClass(".buttonE").click(function () {
+    $("#step4").fadeOut(1000, function () {
+      $("#step5").fadeIn(1000);
+    })
+  });
+
+  $(".buttonE").addClass(".buttonF").click(function () {
+    $("#step5").fadeOut(1000, function () {
+      $("#step6").fadeIn(1000);
+    })
+  });
+
+  // $(".button").click(function () {
+  //   $("#step6").fadeOut(1000, function () {
+  //     $("#finished").fadeIn(1000);
+  //   })
+  // });
+
+
+
 });
-
-
-
 
 
 
