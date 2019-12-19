5819ffa4f13204fcb549036e88e3360a4b999a9e
Jean Lunde <yn5283ne@e2381-as1665816.century.edu>
2019-12-18 19:23:37 -0600

    Commented out button and button interactions.

--- js/script.js
+++ js/script.js
@@ -5,47 +5,47 @@ $(document).ready(function () {
     $('img', this).fadeToggle();
   });
 
-  $(".button").addClass(".buttonA").click(function () {
-    $("#original").fadeOut(1000, function () {
-      $("#step1").fadeIn(1000);
-    })
-  });
+  // $(".button").addClass(".buttonA").click(function () {
+  //   $("#original").fadeOut(1000, function () {
+  //     $("#step1").fadeIn(1000);
+  //   })
+  // });
 
-  $(".buttonA").addClass(".buttonB").click(function () {        
-    $("#step1").fadeOut(1000, function () {
-      $("#step2").fadeIn(1000);
-    })
-  });
+  // $(".buttonA").addClass(".buttonB").click(function () {        
+  //   $("#step1").fadeOut(1000, function () {
+  //     $("#step2").fadeIn(1000);
+  //   })
+  // });
 
-  $(".buttonB").addClass(".buttonC").click(function () {
-    $("#step2").fadeOut(1000, function () {
-      $("#step3").fadeIn(1000);
-    })
-  });
+  // $(".buttonB").addClass(".buttonC").click(function () {
+  //   $("#step2").fadeOut(1000, function () {
+  //     $("#step3").fadeIn(1000);
+  //   })
+  // });
 
-  $(".buttonC").addClass(".buttonD").click(function () {
-    $("#step3").fadeOut(1000, function () {
-      $("#step4").fadeIn(1000);
-    })
-  });
+  // $(".buttonC").addClass(".buttonD").click(function () {
+  //   $("#step3").fadeOut(1000, function () {
+  //     $("#step4").fadeIn(1000);
+  //   })
+  // });
 
-  $(".buttonD").addClass(".buttonE").click(function () {
-    $("#step4").fadeOut(1000, function () {
-      $("#step5").fadeIn(1000);
-    })
-  });
+  // $(".buttonD").addClass(".buttonE").click(function () {
+  //   $("#step4").fadeOut(1000, function () {
+  //     $("#step5").fadeIn(1000);
+  //   })
+  // });
 
-  $(".buttonE").addClass(".buttonF").click(function () {
-    $("#step5").fadeOut(1000, function () {
-      $("#step6").fadeIn(1000);
-    })
-  });
+  // $(".buttonE").addClass(".buttonF").click(function () {
+  //   $("#step5").fadeOut(1000, function () {
+  //     $("#step6").fadeIn(1000);
+  //   })
+  // });
 
-  $(".buttonF").addClass(".buttonF").click(function () {
-    $("#step6").fadeOut(1000, function () {
-      $("#finished").fadeIn(1000);
-    })
-  });
+  // $(".buttonF").addClass(".buttonF").click(function () {
+  //   $("#step6").fadeOut(1000, function () {
+  //     $("#finished").fadeIn(1000);
+  //   })
+  // });
 
 });
 
