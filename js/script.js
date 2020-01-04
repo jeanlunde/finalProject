e6ec6a2dd86a6d3bc67b44626108a028576fca43
Jean Lunde <jean.lunde@gmail.com>
2020-01-03 21:32:50 -0600

    Deleted unused code.

--- js/script.js
+++ js/script.js
@@ -5,71 +5,16 @@ $(document).ready(function () {
     $('img', this).fadeToggle();
   });
 
-  // $(".button").addClass(".buttonA").click(function () {
-  //   $("#original").fadeOut(1000, function () {
-  //     $("#step1").fadeIn(1000);
-  //   })
-  // });
-
-  // $(".buttonA").addClass(".buttonB").click(function () {        
-  //   $("#step1").fadeOut(1000, function () {
-  //     $("#step2").fadeIn(1000);
-  //   })
-  // });
-
-  // $(".buttonB").addClass(".buttonC").click(function () {
-  //   $("#step2").fadeOut(1000, function () {
-  //     $("#step3").fadeIn(1000);
-  //   })
-  // });
-
-  // $(".buttonC").addClass(".buttonD").click(function () {
-  //   $("#step3").fadeOut(1000, function () {
-  //     $("#step4").fadeIn(1000);
-  //   })
-  // });
-
-  // $(".buttonD").addClass(".buttonE").click(function () {
-  //   $("#step4").fadeOut(1000, function () {
-  //     $("#step5").fadeIn(1000);
-  //   })
-  // });
-
-  // $(".buttonE").addClass(".buttonF").click(function () {
-  //   $("#step5").fadeOut(1000, function () {
-  //     $("#step6").fadeIn(1000);
-  //   })
-  // });
-
-  // $(".buttonF").addClass(".buttonF").click(function () {
-  //   $("#step6").fadeOut(1000, function () {
-  //     $("#finished").fadeIn(1000);
-  //   })
-  // });
-  // function show() {
-  // //  $('.declutterWrapper').style.maxHeight = "200px";
-  //   var images = $("img.declutter");
-  //   console.log(images);
-  //   for(var i = 0; i < images.length; i++)
-  //   {
-  //     images[i].src = images[i].getAttribute('data-src');
-  //   }
-  // }
+ 
 
   var arrImages = ['images/declutter-1.png', 'images/declutter-2.png', 'images/declutter-3.png', 'images/declutter-4.png', 'images/declutter-5.png', 'images/declutter-6.png',  'images/declutter-7.png'];
   let imgCount = 0;
-  // let description = $('.declutterDescription');
-  // let desc = description[imgCount];
-  // console.log(description);
-  // console.log('current image ' + arrImages[imgCount]);
+
   $('.button').click(function () {
       imgCount = imgCount + 1;
       if(imgCount < arrImages.length) {
           $('.declutter').fadeIn().attr("src", arrImages[imgCount]);
           
-          // $('.declutterDescription').css('display', 'block');
-          // $('.declutterDescription').css('color', 'red');
-          //console.log('current image ' + arrImages[imgCount]);
           console.log('works');
       }
       else {
