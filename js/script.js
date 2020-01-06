3691c421343ecf4aeeb6f8a28a42d3b9496d7e3b
Jean Lunde <jean.lunde@gmail.com>
2020-01-06 11:28:26 -0600

    Entered some code for the .declutterDescriptions.

--- js/script.js
+++ js/script.js
@@ -1,30 +1,40 @@
 $(document).ready(function () {
-  $("#charactersAll").fadeIn(5000);
+
+    $("#charactersAll").fadeIn(5000);
   $("#charactersSelective").delay(2000).fadeIn(5000);
   $("#beforeAfterCharts").click(function () {
     $('img', this).fadeToggle();
   });
 
- 
 
-  var arrImages = ['images/declutter-1.png', 'images/declutter-2.png', 'images/declutter-3.png', 'images/declutter-4.png', 'images/declutter-5.png', 'images/declutter-6.png',  'images/declutter-7.png'];
+  var arrImages = ['images/declutter-1.png', 'images/declutter-2.png', 'images/declutter-3.png', 'images/declutter-4.png', 'images/declutter-5.png', 'images/declutter-6.png', 'images/declutter-7.png'];
   let imgCount = 0;
+  var arrDescriptions = ["#description1", "#description2", "#description3", "description4", "#description5", "#description6", "#description7"];
+  let descrCount = 0;
 
+  $(".declutterDescription:first-child").css("visibility", "visible");
   $('.button').click(function () {
-      imgCount = imgCount + 1;
-      if(imgCount < arrImages.length) {
-          $('.declutter').fadeIn().attr("src", arrImages[imgCount]);
-          
-          console.log('works');
-      }
-      else {
-          $('.declutter').fadeIn().attr("src", arrImages[0]);
-          imgCount = 0;
-      }
-   })
+    descrCount = descrCount + 1;
+    if (descrCount < arrDescriptions.length) {
+      $(".declutterDescription").css("visibility", "visible");
+    }
+    imgCount = imgCount + 1;
+    if (imgCount < arrImages.length) {
+      $('.declutter').fadeIn().attr("src", arrImages[imgCount]);
+
+      console.log('works');
+    } else {
+      $(".declutterDescription").css("visibility", "hidden");
+      $(".declutterDescription:first-child").css("visibility", "visible");
+      $('.declutter').fadeIn().attr("src", arrImages[0]);
+      descrCount = 0;
+      imgCount = 0;
+    }
+  })
 
   // document.ready function ends here
 });
+
 
 
 
