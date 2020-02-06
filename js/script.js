19266e5956900b7f3cf23a0fe8b58592205c598d
Jean Lunde <jean.lunde@gmail.com>
2020-02-06 16:38:53 -0600

    Added comments to document code.

--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
 $(document).ready(function () {
-
+  // Fade in character background, then selective characters.
   $("#charactersAll").fadeIn(5000);
   $("#charactersSelective").delay(2000).fadeIn(5000);
 
+  // Toggle between before and after images.
   $("#before-after").click(function () {
     $("img", this).fadeToggle();
   });
 
-
+  // Declare variables used for the step-by-step chart progression function.
   var arrImages = ['images/declutter-1.png', 'images/declutter-2.png', 'images/declutter-3.png', 'images/declutter-4.png', 'images/declutter-5.png', 'images/declutter-6.png', 'images/declutter-7.png'];
   let imgCount = 0;
   var arrDescriptions = ["#description1", "#description2", "#description3", "#description4", "#description5", "#description6", "#description7"];
   let descrCount = 0;
 
+  // Make the beginning chart image visible.
   $(".declutterDescription:first-child").css("visibility", "visible");
   $(".button").click(function () {
 
+    // Display each chart image and corresponding description as the button is clicked, while hiding the rest of the images and descriptions.
     if ((descrCount < arrDescriptions.length) && (imgCount < arrImages.length)) {
       descrCount = descrCount + 1;
       $(arrDescriptions[descrCount]).css("visibility", "visible");
